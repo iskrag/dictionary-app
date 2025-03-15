a932c2842420c1da0b2b6dc9c3668a9085e81a48
iskra <iskrag@gmail.com>
2025-03-15 10:20:04 +0100

    Improved functionality. Housekeeping.

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def split_meanings(word):
     meanings = word.split('/')
     for ind, meaning in enumerate(meanings):
         print(f"{ind+1}. " + meaning)
+        pass
 
 
 def generate_swe_word(d):
@@ -25,36 +26,51 @@ def generate_swe_word(d):
     return word
 
 
+def split_guess(guess):
+    """Split guess if guess is more than a single word."""
+    new_guess = [word for word in re.split(r', |; ', guess.strip()) if word]
+    return new_guess
+
+
+def check_translation_or_synonym(d, key, word, guess, syn, counter):
+    if any(word == meaning for meaning in re.split(r', |/', d[key][syn].strip())):
+        print(guess + ' is a correct answer!')
+        counter += 1
+        if len(d[key][syn]) > len(guess):
+            if '/' in d[key][syn]:
+                print('The full answer is: ')
+                split_meanings(d[key][syn].upper())
+            else:
+                print('The full answer is: ', d[key][syn].upper())
+    else:
+        print(word + ' is INCORRECT!')
+        if '/' in d[key][syn]:
+            print('The correct answer is: ')
+            split_meanings(d[key][syn].upper())
+        else:
+            print('The correct answer is: ' + d[key][syn].upper())
+            # if syn == 0:
+                # print('Synonyms: ' + d[key][syn].upper())
+    return counter
+
+
 def check_guess(guess, d, key, syn=0):
     """Check if a guess is correct."""
+    new_guess = split_guess(guess)
     counter = 0
-    split_guess = [word for word in re.split(r', |; ', guess.strip()) if word]
-
-    if not split_guess:  # If user input was empty or only spaces
+    if not new_guess:  # If user input was empty or only spaces
         print("INCORRECT! No valid answer given.")
         print('The correct answer is: ' + d[key][0].upper() if syn == 0 else
               d[key][1].upper())
         return counter
 
-    for word in split_guess:
+    for word in new_guess:
         if syn == 0:
-            if word in d[key][0]:
-                print(word + ' is a correct answer!')
-                counter += 1
-            else:
-                print(word + ' is INCORRECT!')
-                print('The correct answer is: ' + d[key][0].upper())
-                return counter
+            counter = check_translation_or_synonym(d, key, word, guess, syn, counter)
+            return counter
         elif syn == 1:
-            if word in d[key][1]:
-                print(len(d[key][1]))
-                print(word + ' is a correct answer!')
-                counter += 1
-            else:
-                print(word + ' is INCORRECT.')
-                print('The correct answer is: ' + d[key][1].upper())
-                return counter
-    return counter
+            counter = check_translation_or_synonym(d, key, word, guess, syn, counter)
+            return counter
 
 
 def process_dict(dict_file):
@@ -67,9 +83,6 @@ def process_dict(dict_file):
                 continue  # Skip malformed lines
             swe_word, eng_word = clean_word(split_line[0]), clean_word(split_line[1])
             words_dict[swe_word] = [eng_word]
-            # print(swe_word, eng_word)
-            # if '/' in line:
-            #     split_meanings(eng_word)
             if ';' in line:
                 syn = clean_word(split_line[2])
                 words_dict[swe_word].append(syn)
@@ -86,7 +99,6 @@ while True:
     print('Swedish word: ' + random_word.upper())
     eng_guess = input('English translation: ')
     count = check_guess(eng_guess, words_dict, random_word)
-
     if count > 0 and len(words_dict[random_word]) > 1:
         synonym = input('Enter synonym: ')
         check_guess(synonym, words_dict, random_word, syn=1)
